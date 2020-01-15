e1a246eacbdf39874e4417fd309cadcba6d1e00b
wchorkaw <wchorkaw@ualberta.ca>
2020-01-15 13:52:21 -0700

    Lecture Code

---
+++ Will/prog-01-01.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : lecture-01.c
+ Author      : mr
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+
+int main(void) {
+
+	int num;
+	num = 7;
+
+	printf("I am a simple computer.\n");
+	printf("My favorite number is %d.\n", num);
+
+	return 0;
+}
