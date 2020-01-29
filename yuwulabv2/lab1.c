971e8d56c3b988d6ae449d570b50e1697da27df1
charlesancheta <cancheta@ualberta.ca>
2020-01-28 18:49:50 -0700

    finished soln

---
+++ yuwulabv2/lab1.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+// defines boolean type using enum
+typedef enum {true, false} bool;
+void main() {
+  // uses current time as random seed
+  srand(time(0));
+  int counter = 0;
+  int min, max, option;
+  printf("Range: minimum ");
+  scanf("%d", &min);
+  printf("Range: maximum ");
+  scanf("%d", &max);
+  bool guessed = false;
+  while (guessed == false) {
+    int program_guessed_number = (min + max)/2;
+    printf("Guessed number: %d\n", program_guessed_number);
+    printf("1. TOO LARGE\n");
+    printf("2. TOO SMALL\n");
+    printf("3. FOUND\n");
+    scanf("%d", &option);
+    switch (option) {
+      case 1:
+        max = program_guessed_number + 1;
+        counter++;
+        break;
+      case 2:
+        min = program_guessed_number - 1;
+        counter++;
+        break;
+      case 3:
+        guessed = true;
+        break;
+      default:
+        break;
+    }
+  }
+  printf("FOUND IT\nNo of tries: %d", counter);
+}
