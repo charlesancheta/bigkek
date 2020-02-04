b3903f35664a74f6bfa4c92d0f5a9860fe368e74
charlesancheta <55412395+charlesancheta@users.noreply.github.com>
2020-02-04 15:36:35 -0700

    Update Lab1a.s

--- Lab1a.s
+++ Lab1a.s
@@ -11,31 +11,60 @@ movem.l %d2-%d7/%a2-%a5,(%a7)
 /******************************************************************/
 /* General Information ********************************************/
 /* File Name: Lab1a.s *********************************************/
-/* Names of Students: _________________ and ____________________ **/
-/* Date: _________________                                       **/
+/* Names of Students: Charles Ancheta and Mustafa Farow          **/
+/* Date: February 4, 2020                                        **/
 /* General Description:                                          **/
 /*                                                               **/
 /******************************************************************/
 
 /*Write your program here******************************************/
+move.l #0x43000000, %a2
+move.l #0x43100000, %a3
+
+Repeat:
+move.l %a2, %d2
+cmpi.l #0xd, %d2
+beq Exit
+cmp.l #0x30, %d2
+blt Error
+cmp.l #0x39, %d2
+bgt Uppercase
+sub.l #0x30, %d2
+move.l %d2, %a3
+add.l #4, %a2
+add.l #4, %a3
+bra Repeat
+
+Uppercase:
+cmp.l #0x41, %d2
+blt Error
+cmp.l #0x46, %d2
+bgt Lowercase
+sub.l #0x31, %d2
+move.l %d2, %a3
+add.l #4, %a2
+add.l #4, %a3
+bra Repeat
+
+Lowercase:
+cmp.l #0x61, %d2
+blt Error
+cmp.l #0x66, %d2
+bgt Error
+sub.l #0x61, %d2
+move.l %d2, %a3
+add.l #4, %a2
+add.l #4, %a3
+bra Repeat
 
 
+Error:
+move.l 0xFFFFFFFF, %a3
+add.l #4, %a2
+add.l #4, %a3
+bra Repeat
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+Exit:
 /*End of program **************************************************/
 
 /* DO NOT MODIFY THIS --------------------------------------------*/
