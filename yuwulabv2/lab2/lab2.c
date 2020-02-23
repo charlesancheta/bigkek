8ba94d909655668d5ecff1af1c11a788196e30a1
charlesancheta <cancheta@ualberta.ca>
2020-02-23 10:21:49 -0700

    im dead

---
+++ yuwulabv2/lab2/lab2.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <locale.h>
+
+#define ERR -1
+#define FROM_SIZE 3
+#define TO_SIZE   4
+
+int main(void) {
+    
+    FILE* fkey;
+    FILE* fin;
+    FILE* fout;
+
+    setlocale(LC_ALL, "en_US.UTF-8");
+    
+    // replace paths (below) with the paths to files at your machine
+    char fkey_path[] = "/home/charles/COMPELYF/bigkek/yuwulabv2/Key.dat";
+    char fin_path[]  = "/home/charles/COMPELYF/bigkek/yuwulabv2/Raw.dat";
+    char fout_path[] = "/home/charles/COMPELYF/bigkek/yuwulabv2/output.dat";
+       
+    unsigned char buf_in[FROM_SIZE];
+    unsigned char buf_out[TO_SIZE];
+    char keys[64];
+    int count = 0;
+    int total_char = 0;
+
+    // cLion/xCode version
+    fkey=fopen(fkey_path,"r");
+    if (fkey == NULL) {
+        printf("The file cannot be found\n");
+        return ERR;
+    }
+    fread(keys, FROM_SIZE, 64, fkey);
+    // closing file
+    fclose(fkey);
+    
+    // cLion/xCode version
+    fin=fopen(fin_path,"r");
+    if (fin == NULL) {
+        printf("The file cannot be found\n");
+        return ERR;
+    }
+
+    // cLion/xCode version
+    fout=fopen(fout_path,"w");
+    if (fout == NULL) {
+        printf("The file cannot be found\n");
+        exit(ERR);
+    }
+    
+    // Conversion from ASCII to Base64
+    fread(buf_in, 1, 2, fin);
+    while (buf_in != '\0') {
+        
+    } 
+    
+    
+    // Saving Base64 text into a file
+        
+
+    
+    // closing files
+    fclose(fin);
+    fclose(fout);
+
+    return 0;
+}
