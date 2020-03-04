addba0d991c66895f3eed12d2c627f306b928c6a
charlesancheta <cancheta@ualberta.ca>
2020-03-03 22:46:51 -0700

    done lab 2 i mean

--- assignments/a1q2.c
+++ assignments/a1q2.c
@@ -2,28 +2,24 @@
 #include <stdlib.h>
 
 int roll() {
-  return 2 * (rand() % 3) + 1;
-}
-
-int testroll() {
-  for (int i = 0; i < 10; i++){
-    printf("%d\n", roll());
-  }
+  return 2 * (rand() % 3) + 1; // rolls either 1, 3, or 5
 }
 
 int main() {
-  srand(time(0));
-  int x, rollsum, freqs[9] = {};
-  unsigned int sum = 0;
+  srand(time(0)); // uses current time as random seed
+  int x; // initialize number of rols
+  int rollsum;
+  int freqs[5] = {}; // initializes array of zeros
+  unsigned int sum = 0; // sum to calculate mean
   scanf("%d", &x);
   for (int i = 0; i < x; i++) {
-    rollsum = roll() + roll();
-    freqs[rollsum - 2]++;
+    rollsum = roll() + roll(); // rolls two dice
+    freqs[rollsum/2 - 1]++;
     sum += rollsum;
   }
   printf("Sum  Frequency\n");
-  for (int i = 0; i < 9; i++) {
-    printf("%2d %8d\n", i+2, freqs[i]);
+  for (int i = 0; i < 5; i++) {
+    printf("%2d %8d\n", 2*(i+1), freqs[i]);
   }
   printf("Mean value is: %d\n", sum/x);
   return 0;
